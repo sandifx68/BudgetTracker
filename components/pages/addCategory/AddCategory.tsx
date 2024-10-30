85ed3831b37598085ed1ef5ae625eb1e7071714c
sandifx68 <sandixd333@gmail.com>
2024-10-30 23:14:55 +0000

    Categories have images now, still need to show them on the pie chart

--- components/pages/addCategory/AddCategory.tsx
+++ components/pages/addCategory/AddCategory.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text, Pressable, KeyboardAvoidingView, StyleSheet, Platform } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Platform,
+  LayoutChangeEvent,
+} from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import Toast from "react-native-toast-message";
 import { useNavigation } from "@react-navigation/native";
-import { TextInput } from "react-native-gesture-handler";
+import { FlatList, TextInput } from "react-native-gesture-handler";
+import { imageData, ImgData } from "../../../assets/categoryImages/imageData";
 
 export function HeaderRightComponentAddCategory({ category }: any): React.JSX.Element | undefined {
   const db = useSQLiteContext();
@@ -12,6 +21,10 @@ export function HeaderRightComponentAddCategory({ category }: any): React.JSX.El
   const deleteCategory = () => {
     db.runSync("DELETE FROM categories WHERE id = ?", category.id);
     navigation.navigate("Expense List");
+    Toast.show({
+      type: "success",
+      text1: "Category successfully deleted!",
+    });
   };
 
   if (category)
@@ -23,32 +36,38 @@ export function HeaderRightComponentAddCategory({ category }: any): React.JSX.El
     );
 }
 
+const imageGridBorderWidth = 5;
+
 export function AddCategory({ route, navigation }: any): React.JSX.Element {
   const db = useSQLiteContext();
   const [category, setCategory] = React.useState<Category>();
+  const [imageGridWidth, setImageGridWidth] = React.useState<number>();
+  const imagesPerRow = 3;
+  const highlightedImageColor = "#FF0000";
 
   React.useEffect(() => {
-    setCategory(route.params?.category);
+    const categoryToEdit = route.params?.category;
+    setCategory(categoryToEdit);
   }, [route]);
 
-  const addCategory = (name: string) => {
-    db.runSync("INSERT INTO categories (name) VALUES (?)", name);
+  const addCategory = (name: string, image_id: number) => {
+    db.runSync("INSERT INTO categories (name, image_id) VALUES (?,?)", name, image_id);
   };
 
-  const updateCategory = (name: string, id: number) => {
-    db.runSync("UPDATE categories SET name = ? WHERE id = ?", name, id);
+  const updateCategory = (name: string, image_id: number, id: number) => {
+    db.runSync("UPDATE categories SET name = ?, image_id = ? WHERE id = ?", name, image_id, id);
   };
 
   const handleAddCategory = () => {
     if (category?.name) {
       if (!category.id) {
-        addCategory(category.name);
+        addCategory(category.name, category.image_id);
         Toast.show({
           type: "success",
           text1: "Category successfully added!",
         });
       } else {
-        updateCategory(category.name, category.id);
+        updateCategory(category.name, category.image_id, category.id);
         Toast.show({
           type: "info",
           text1: "Category successfully modified!",
@@ -60,6 +79,34 @@ export function AddCategory({ route, navigation }: any): React.JSX.Element {
         type: "error",
         text1: "Category name can't be empty!",
       });
+    }
+  };
+
+  const setCategoryImage = (imageNumber: number) => {
+    setCategory({ ...(category as Category), image_id: imageNumber });
+  };
+
+  const renderImage = (imgData: ImgData) => {
+    if (imageGridWidth) {
+      const sizePerItem = imageGridWidth / imagesPerRow;
+      const CategoryImage: any = imgData.source;
+      return (
+        <CategoryImage
+          height={sizePerItem}
+          width={sizePerItem}
+          fill={imgData.id == category?.image_id ? highlightedImageColor : "#000000"}
+          onPress={() => setCategoryImage(imgData.id)}
+        />
+      );
+    }
+    return null;
+  };
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const onLayoutWidth = event.nativeEvent.layout.width;
+
+    if (onLayoutWidth > 0 && imageGridWidth !== onLayoutWidth) {
+      setImageGridWidth(onLayoutWidth - imageGridBorderWidth * 2);
     }
   };
 
@@ -82,6 +129,14 @@ export function AddCategory({ route, navigation }: any): React.JSX.Element {
             <Text style={styles.addText}> {!category ? "Add Category!" : "Modify Category!"} </Text>
           </View>
         </Pressable>
+      </View>
+
+      <View style={styles.imageGridWrapper} onLayout={onLayout}>
+        <FlatList
+          data={imageData}
+          renderItem={({ item }) => renderImage(item)}
+          numColumns={imagesPerRow}
+        />
       </View>
     </View>
   );
@@ -121,5 +176,13 @@ const styles = StyleSheet.create({
     borderRadius: 60,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageGridWrapper: {
+    marginTop: 15,
+    borderColor: "dimgray",
+    borderWidth: imageGridBorderWidth,
+    borderRadius: 10,
+    height: "50%",
+    width: "80%",
   },
 });
--- models/Category.tsx
+++ models/Category.tsx
@@ -1,5 +1,6 @@
 interface Category {
   id: number;
   name: string;
+  image_id: number;
   is_selected: boolean;
 }
